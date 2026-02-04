d8d759de8bd2dcc692b28aa7c657b044c3734018
Ayush <ayushchoudhary082003@gmail.com>
2026-02-04 10:50:08 +0530

    add tailwund css

---
+++ cssproject/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Headers from "./components/Headers";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-800">hello bhai</h1>
+      <Headers />
+    </div>
+  );
+};
+
+export default App;
---
+++ cssproject/src/components/Headers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Headers = () => {
+  return (
+    <div>
+      <h1>I am haeder </h1>
+    </div>
+  );
+};
+
+export default Headers;
